047d789cac42bbcc71d80a499055abfe18ff32e9
程二枫 <369495368@qq.com>
2016-03-06 23:45:08 +0800

    pod

--- BaseProject/BaseController/JKBaseNavigator.h
+++ BaseProject/BaseController/JKBaseNavigator.h
@@ -6,15 +6,13 @@
 //  Copyright © 2016年 宇之楓鷙. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
 #import "JKURLAction.h"
 #import "JKCoreNavigationViewController.h"
 
 
 @interface JKBaseNavigator : NSObject
-
-
-
 
 
 @end
---
+++ Podfile
@@ -0,0 +1,9 @@
+#source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '7.0'
+inhibit_all_warnings!
+
+pod 'AFNetworking'
+
+
+pod 'UMengAnalytics', '~> 3.6.6'
